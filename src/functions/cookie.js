2625d6703d2ab0a704604c9f3994a3c14dbb199d
unclechuen <johnny@johnnyip.com>
2023-01-17 17:59:04 +0800

    Minor

--- src/functions/cookie.js
+++ src/functions/cookie.js
@@ -12,10 +12,10 @@ export async function getCookies() {
 
     var cookies = document.cookie.split(";")
     for (var i = 0; i < cookies.length; i++) {
-        while (cookies[i].charAt(0) == ' ') {
+        while (cookies[i].charAt(0) === ' ') {
             cookies[i] = cookies[i].substring(1);
         }
-        if (cookies[i].indexOf("downloaded") == 0) {
+        if (cookies[i].indexOf("downloaded") === 0) {
             var codeSaved = cookies[i].split('=')[1]
             console.log("Cookie found: " + codeSaved)
             return codeSaved;
